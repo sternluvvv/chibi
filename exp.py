7601f8640601332d022307e6af859c5b49bb13a2
HIRAMATSU Mamika <21716077@jwu.ac.jp>
2019-10-28 16:48:44 +0900

    parser3

--- exp.py
+++ exp.py
@@ -20,6 +20,10 @@ def toExpr(a):
     return a
 
 class Binary(Expr):
+    __slots__=['left','right']
+    def __init__(self,a,b):
+        self.left = toExpr(a)
+        self.right = toExpr(b)
     def __repr__(self):
         cname = self.__class__.__name__
         return f'{cname}({self.left},{self.right})'
@@ -62,4 +66,6 @@ class Div(Binary):
         self.right = toExpr(b)
     def eval(self):
         return self.left.eval() // self.right.eval()
+
+
 
--- parser.py
+++ parser.py
@@ -1,29 +1,24 @@
-from exp import Val
+from exp import Val,Sub,Add,Mul,Div
 
 def parse(s: str):
-        num = int(s)
-        return Val(num)
-
-e = parse("123")
-print(e)
-
-s = "1+2"
-pos = s.find('+')
-print('pos',pos)
-
-s1 = s[0:pos]
-s2 = s[pos+1:]
-print(s,s1,s2)
-
-def parse(s:str):
-    pos = s.find('+')
-    if pos == -1:
-        num = int(s)
-        return Val(num)
-    else :
+    if s.find('+') > 0:
+        pos = s.find('+')
         s1 = s[0:pos]
         s2 = s[pos+1:]
-        return Add(Val(int(s1)),Val(int(s2)))
+        return Add(parse(s1),parse(s2))
+    if s.find('*') > 0:
+        pos = s.find('*')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Mul(parse(s1),parse(s2))
+    if s.find('-') > 0 :
+        pos = s.find('-')
+        s1 = s[0:pos]
+        s2 = s[pos+1:] 
+        return Sub(parse(s1),parse(s2))    
 
-e = parse("123456")
-print(e)
+    return Val(int(s))
+
+e = parse("1-2-3")
+print(e, e.eval())
+assert e.eval() == -4
