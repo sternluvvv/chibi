3e35a88201665ae66d882ec139437b7ea6fcf743
HIRAMATSU Mamika <21716077@jwu.ac.jp>
2019-12-02 16:27:11 +0900

    sternluvvv

--- icalc.py
+++ icalc.py
@@ -1,13 +1,11 @@
 import pegpy
 
-peg = pegpy.grammar('math.tpeg')
+peg = pegpy.grammar('''
+Expression = Product (^{ '+' Product #Add })*
+Product = Value (^{ '*' Value #Mul })*
+Value = { [0-9]+ #Int }
+''')
 parser = pegpy.generate(peg)
-
-t = parser('1+2*3')
-print(repr(t))
-
-t = parser('@2')
-print(repr(t))
 
 def calc(t):
     if t == 'Int' :
@@ -19,6 +17,17 @@ def calc(t):
     print(f'TODO{t.tag}')
     return 0
 
-t = parser('1+2*3+4*5')
-print(repr(t))
-print(calc(t))
+#t = parser('1+2*3+4*5')
+#print(repr(t))
+#print(calc(t))
+
+def runb(s:str):
+    print('TODO',s)
+    
+def main() :
+    s = input('>>> ')
+    t = parser(s)
+    print(calc(t))
+
+if __name__ == '__main__':
+    main()
